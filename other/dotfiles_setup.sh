e88c7d892d9c9a27c621ec5bc37c349d3403c641
Phil <Philip.Shaughnessy@gmail.com>
2018-12-20 06:26:27 -0500

    delete unused

--- other/dotfiles_setup.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-dotfiles=("vimrc" "zshrc" "tmux.conf")
-dir=${HOME}/dotfiles
-for dotfile in ${dotfiles[@]}; do
- ln -sf ${dir}/${dotfile} ${HOME}/.${dotfile}
-done
-
-ln -sf ${dir}/vimrc ~/.config/nvim/init.vim
